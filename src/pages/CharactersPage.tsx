53d083fd9a4cc8740e997f1e3902f7e0f86238a3
Vincenzo Pierro <vcp@icure.com>
2024-12-25 11:12:48 +0100

    Added characters history to main page

--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -1,23 +1,27 @@
-import { Alert, AlertIcon, Center, AlertTitle, AlertDescription } from "@chakra-ui/react";
-import { useGetCurrentActiveCharactersQuery } from "../services/character";
+import {Alert, AlertIcon, Center, AlertTitle, AlertDescription, useBreakpointValue} from "@chakra-ui/react";
+import {useGetAllCharactersForSelfQuery} from "../services/character";
 import { CharacterList } from "../components/character/CharacterList";
 
 export const CharactersPage = () => {
-    const { data: characters, error: charactersError} =
-        useGetCurrentActiveCharactersQuery();
 
-    return (
-        <Center>
-            <CharacterList characters={characters} />
-            {!!charactersError && (
-                <Alert status="error">
-                    <AlertIcon />
-                    <AlertTitle>There was an error while loading your characters</AlertTitle>
-                    <AlertDescription>
-                        {JSON.stringify(charactersError)}
-                    </AlertDescription>
-                </Alert>
-            )}
-        </Center>
-    );
+	const { data: characters, error: charactersError} =
+		useGetAllCharactersForSelfQuery();
+
+	const activeCharacters = characters?.filter(it => it.status === "active" || it.status == null)
+	const otherCharacters = characters?.filter(it => it.status !== "active" && it.status != null)
+
+	return (
+		<Center>
+			<CharacterList activeCharacters={activeCharacters} otherCharacters={otherCharacters} />
+			{!!charactersError && (
+				<Alert status="error">
+					<AlertIcon />
+					<AlertTitle>There was an error while loading your characters</AlertTitle>
+					<AlertDescription>
+						{JSON.stringify(charactersError)}
+					</AlertDescription>
+				</Alert>
+			)}
+		</Center>
+	);
 };
